2a305f9d656d2b54f3d49264ea168778ff02339e
DoubleBrackets <arthurgan29@gmail.com>
2019-09-06 23:18:18 -0700

    Add files via upload

---
+++ junk.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class junk {
+	public static void main(String[] args)
+	{
+		// Calculate pay at work based on hours worked each day 4 
+		int hours = (4 + 5 + 8 +4);
+		System.out.println("My total hours worked: \n" + hours + "\nMy hourly salary:\n$8.75\nMy total pay:\n"+ (hours * 8.75) + 
+				"\nMy taxes owed:\n" + (hours * 8.75 * 0.20)); 
+
+	}
+}
